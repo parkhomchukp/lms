2664dfd93da4922f3d980dd8d076f718274c01f9
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-10-06 23:21:11 +0300

    Upgraded Student class

--- student/models.py
+++ student/models.py
@@ -1,11 +1,33 @@
 import datetime
+from faker import Faker
 
 from django.db import models
 
 
 # Create your models here.
 class Student(models.Model):
-    first_name = models.CharField(max_length=80, null=False)
+    first_name = models.CharField(max_length=60, null=False)
     last_name = models.CharField(max_length=80, null=False)
     email = models.EmailField(max_length=120, null=True)
-    birthday = models.DateField(null=True, default=datetime.datetime)
+    birthdate = models.DateField(null=True, default=datetime.date.today)
+
+    def __str__(self):
+        return f'{self.full_name()}, {self.age()} ({self.id})'
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def age(self):
+        return datetime.datetime.now().year - self.birthdate.year
+
+    @classmethod
+    def generate_instances(cls, count):
+        faker = Faker()
+        for _ in range(count):
+            st = cls(
+                first_name=faker.first_name(),
+                last_name=faker.last_name(),
+                email=faker.email(),
+                birthdate=faker.date_time_between(start_date="-30y", end_date="-18y"),
+            )
+            st.save()
