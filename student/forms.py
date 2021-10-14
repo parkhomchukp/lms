efed9d6b32686ca0c39876c4b850334c70bc4cb7
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-10-14 16:55:02 +0300

    Done fourth assignment

--- student/forms.py
+++ student/forms.py
@@ -7,7 +7,7 @@ from student.models import Student
 class StudentCreateForm(ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'birthdate']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'birthdate', 'enroll_date', 'graduate_date']
 
         widgets = {'phone_number': TextInput(attrs={'pattern': '\d{10,14}'})}
 
@@ -32,5 +32,10 @@ class StudentCreateForm(ModelForm):
         last_name = cleaned_data['last_name']
         if first_name == last_name:
             raise ValidationError('First and last names can\'t be equal')
+
+        enroll_date = cleaned_data['enroll_date']
+        graduate_date = cleaned_data['graduate_date']
+        if enroll_date >= graduate_date:
+            raise ValidationError('Enroll date must be less then graduate date')
 
         return cleaned_data
