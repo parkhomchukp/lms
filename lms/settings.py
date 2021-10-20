898461bbbcdfd0c31fe0acbb87070f1081d55954
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-10-20 19:51:38 +0300

    Hid SECRET_KEY

--- lms/settings.py
+++ lms/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!h2u#e%=4x)(g^1pk)_+dban8t4t00!5+nxq$2#)fgh=kq6hu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
