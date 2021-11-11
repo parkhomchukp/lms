a21bafab6ad422cb932ee9411958f246719cf8de
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-11-11 11:42:22 +0200

    Fixed bugs

--- lms/urls.py
+++ lms/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from student.views import hello, index, IndexView
+from student.views import IndexView
 from django.conf.urls.static import static
 from django.conf import settings
 
