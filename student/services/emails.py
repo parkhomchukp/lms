f9f4a48958b7c0810553325d0c74c6b744ef3274
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-11-15 13:22:35 +0200

    Deleted unused lines

--- student/services/emails.py
+++ student/services/emails.py
@@ -22,7 +22,7 @@ def send_registration_email(request, user_instance):
     email = EmailMessage(
         subject=mail_subject,
         body=message,
-        to=["students.lms.3@gmail.com"],
+        to=["user.email"],
     )
     email.content_subtype = "html"
     email.send(fail_silently=False)
--- student/urls.py
+++ student/urls.py
@@ -43,9 +43,7 @@ urlpatterns = [
     path("login/", UserLogin.as_view(), name="login"),
     path("registration/", RegistrationStudent.as_view(), name="registration"),
     path("logout/", UserLogout.as_view(), name="logout"),
-    path("send-email/", send_email, name="send_email"),
     path("activate/<str:uuid64>/<str:token>", ActivateUser.as_view(), name="activate"),
-    path("test/", test_view, name="test"),
 ]
 
 handler404 = Error404.as_view()
