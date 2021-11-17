ff4e0678f8c0b380bd25e89cbe9db3f57f6453ae
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2021-11-17 15:40:08 +0200

    Removed groups app

--- groups/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from groups.models import Group
-
-
-class GroupCreateForm(ModelForm):
-    class Meta:
-        model = Group
-        fields = ['start_date', 'group_name', 'number_of_members']
